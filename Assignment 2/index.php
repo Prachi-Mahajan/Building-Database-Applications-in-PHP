759785d503f7473af713989f540983200de8ef55
Prachi-Mahajan <prachimahajan59@gmail.com>
2020-09-28 23:23:15 +0530

    h

---
+++ Assignment 2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Prachi Mahajan</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Autos Database Application</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+</div>
+</body>
